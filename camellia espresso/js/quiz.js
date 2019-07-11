6c03a44f62875ffa91644a39011e60e931db6d17
Yashen Raveesha Samarasingha <45714091+yashen97@users.noreply.github.com>
2019-07-11 13:10:50 +0530

    For quiz

--- camellia espresso/js/quiz.js
+++ camellia espresso/js/quiz.js
@@ -1,1 +1,69 @@
-document.write("Hello World !");
+function blood()
+{   
+  var a = document.getElementById("bp");
+  var b = document.getElementById("blood");
+  if(a.checked == true)
+  {
+    b.style.display = "block";
+  }
+  else
+  {
+    b.style.display = "none";
+  }
+}
+
+function sugar()
+{
+  var a = document.getElementById("db");
+  var b = document.getElementById("sugar");
+  if(a.checked == true)
+  {
+    b.style.display = "block";
+  }
+  else
+  {
+    b.style.display = "none";
+  }
+}
+
+function weight()
+{
+  var a = document.getElementById("wl");
+  var b = document.getElementById("weight");
+  if(a.checked == true)
+  {
+    b.style.display = "block";
+  }
+  else
+  {
+    b.style.display = "none";
+  }
+}
+
+function cancer()
+{
+  var a = document.getElementById("cc");
+  var b = document.getElementById("cancer");
+  if(a.checked == true)
+  {
+    b.style.display = "block";
+  }
+  else
+  {
+    b.style.display = "none";
+  }
+}
+
+function beauty()
+{
+  var a = document.getElementById("bt");
+  var b = document.getElementById("beauty");
+  if(a.checked == true)
+  {
+    b.style.display = "block";
+  }
+  else
+  {
+    b.style.display = "none";
+  }
+}
